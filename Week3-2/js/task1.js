ea84b4455558d794c3b468a7e4d5abafdc828dfa
Sokhchamroeun <kolsokhchamroeun20763@gmail.com>
2016-04-07 17:13:39 +0700

    Week 3

--- Week3-2/js/task1.js
+++ Week3-2/js/task1.js
@@ -1,10 +1,26 @@
+(function(){ // on load event
 var task1 = {
 	render: function(){
 		document.getElementById("screenX").textContent = window.screenX;
 		document.getElementById("screenY").textContent = window.screenY;
 		document.getElementById("screenWidth").textContent = window.innerWidth
 		document.getElementById("screenHeight").textContent = window.innerHeight;
-		document.getElementById("location").textContent = window.location;		
+		document.getElementById("location").textContent = window.location.pathname;		
+	},
+	resize: function(){
+		window.addEventListener("resize", this.render);
+	},
+	devinfo: function(){
+		var btnDev = document.getElementById("dev-info");
+		btnDev.addEventListener("click", function(){
+		var devInfo = "<br>" + "Sokhchamroeun" + "<br>" + "Faculty of Science and Technology" + "<br>" + "<a href='https://github.com/Sokhchamroeun'>https://github.com/Sokhchamroeun</a>";
+		var devInformation = document.getElementById("dev-info-container");
+		devInformation.innerHTML = devInfo;
+
+	});
 	}
 }
 task1.render();
+task1.resize();
+task1.devinfo();
+})(); // end on load events
